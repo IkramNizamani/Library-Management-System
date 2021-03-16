6354106d9904c42e778704a7f8201c21190b8498
Muhammad Ikram <nizamaniikram00@gmail.com>
2021-03-16 23:54:07 +0500

    Prepare for deployment

--- backend/seeder.js
+++ backend/seeder.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose'
 import colors from 'colors'
 import dotenv from 'dotenv'
 import users from './data/users.js'
-// import products from './data/products.js'
 import User from './models/userModel.js'
-// import Product from './models/productModel.js'
-// import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
@@ -15,15 +12,8 @@ connectDB()
 const importData = async () => {
   try {
     await User.deleteMany()
-    // await Product.deleteMany()
-    // await Order.deleteMany()
     const createdUsers = await User.insertMany(users)
     const adminUser = createdUsers[0]._id
-
-    // const sampleProduct = products.map((product) => {
-    //   return { ...product, user: adminUser }
-    // })
-    // await Product.insertMany(sampleProduct)
 
     console.log('Data Imported!'.green.inverse)
     process.exit()
@@ -36,8 +26,6 @@ const importData = async () => {
 const destroyData = async () => {
   try {
     await User.deleteMany()
-    // await Product.deleteMany()
-    // await Order.deleteMany()
     console.log('Data Destroyed!'.red.inverse)
     process.exit()
   } catch (error) {
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-// import {
-//   productListReducer,
-//   productDetailsReducer,
-//   productDeleteReducer,
-//   productCreateReducer,
-//   productUpdateReducer,
-// } from './reducers/productReducers'
-// import { cartReducer } from './reducers/cartReducers'
 import {
   userLoginReducer,
   userRegisterReducer,
@@ -20,20 +12,8 @@ import {
   userAddBookReducer,
   userBookListReducer,
 } from './reducers/userReducers'
-// import {
-//   orderCreateReducer,
-//   orderDetailsReducer,
-//   orderPayReducer,
-//   orderListMyReducer,
-// } from './reducers/orderReducers'
 
 const reducer = combineReducers({
-  // productList: productListReducer,
-  // productDetails: productDetailsReducer,
-  // productDelete: productDeleteReducer,
-  // productCreate: productCreateReducer,
-  // productUpdate: productUpdateReducer,
-  // cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
@@ -43,15 +23,7 @@ const reducer = combineReducers({
   userUpdate: userUpdateReducer,
   userAddBook: userAddBookReducer,
   userBookList: userBookListReducer,
-  // orderCreate: orderCreateReducer,
-  // orderDetails: orderDetailsReducer,
-  // orderPay: orderPayReducer,
-  // orderListMy: orderListMyReducer,
 })
-
-// const cartItemsFromStorage = localStorage.getItem('cartItems')
-//   ? JSON.parse(localStorage.getItem('cartItems'))
-//   : []
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
@@ -61,15 +33,7 @@ const bookInfoFromStorage = localStorage.getItem('books')
   ? JSON.parse(localStorage.getItem('books'))
   : null
 
-// const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
-//   ? JSON.parse(localStorage.getItem('shippingAddress'))
-//   : {}
-
 const initialState = {
-  // cart: {
-  //   cartItems: cartItemsFromStorage,
-  //   shippingAddress: shippingAddressFromStorage,
-  // },
   userLogin: { userInfo: userInfoFromStorage },
   userAddBook: { books: bookInfoFromStorage },
 }
